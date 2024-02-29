2aeff3d40c523abf24a956da20eda80302647dd5
vzkstrx <154731072+vzkstrx@users.noreply.github.com>
2024-02-29 12:05:55 +0300

    Update CameraZoom.cs

--- Assets/Scripts/Player/PlayerCamera/CameraZoom.cs
+++ Assets/Scripts/Player/PlayerCamera/CameraZoom.cs
@@ -21,6 +21,7 @@ public class CameraZoom : MonoBehaviour
 
     [Inject]
     private void Construct(InputSystem inputSystem) => _inputSystem = inputSystem;
+    
     private void OnEnable()
     {
         _inputSystem.Player.CameraZoom.performed += ChangeFOV;
